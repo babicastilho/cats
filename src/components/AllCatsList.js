2bce6a45cf37d1b9a11c3eccb1b39f9ccc5897a2
Barbara Castilho <barbara.castilho@outlook.com.br>
2022-12-09 14:28:16 +0000

    two

--- src/components/AllCatsList.js
+++ src/components/AllCatsList.js
@@ -5,7 +5,7 @@ export default class AllCatsList extends Component {
     render() {
         const { color } = this.props.cat;
         return (<div className={`w-80 p-6 border-white rounded-md border-2 bg-[${color}] flex gap-4`} style={{ backgroundColor: color }}>
-        <img src={this.props.cat.image} alt="Cat's image" className="w-[128px] h-100 rounded-full"/>
+        
         <div className="flex flex-col">
           <h2>{this.props.cat.name}</h2>
           <p>
--- src/components/NeighborsCatsList.js
+++ src/components/NeighborsCatsList.js
@@ -3,8 +3,7 @@ export default class AllCatsList extends Component {
     render() {
         const { color } = this.props.cat;
         return (<div className={`w-80 p-4 border-white rounded-md border-2 bg-[${color}] flex gap-4`} style={{ backgroundColor: color }}>
-        <img src={this.props.cat.image} alt="Cat's image" className="w-[128px] rounded-full"/>
-        <div className="flex flex-col">
+         <div className="flex flex-col">
           <h2>{this.props.cat.name}</h2>
           <p>{this.props.cat.age} years old</p>
         </div>
